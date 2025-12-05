a4da724d6ad30eff636b10f20ea3861b7333338f
Sebastian Will <sebastian.h.will@gmail.com>
2025-12-05 16:26:31 +0100

    Add GitHub Pages deployment workflow and SPA routing support

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+// Handle GitHub Pages SPA redirect
+const params = new URLSearchParams(window.location.search)
+const redirectPath = params.get('p')
+if (redirectPath) {
+  window.history.replaceState(null, '', decodeURIComponent(redirectPath))
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
